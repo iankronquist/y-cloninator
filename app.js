ac17e30108a737376205957950745dc9a004783b
Ian Kronquist <iankronquist@teleport.com>
2014-12-29 16:23:11 -0800

    Reorganize application entry point

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-"use strict"
+'use strict'
+var express = require('express');
 var nunjucks = require('nunjucks');
 var knex = require('knex')({
-  client: 'pg',
-  connection: process.env.DATABASE_URL
+  client: process.env.Client || 'sqlite3',
+  connection: process.env.DATABASE_URL || { filename: 'dev.sqlite3' }
 });
-var express = require('express');
-var app = express();
+var bookshelf = require('bookshelf')(knex);
 
+var app = express();
 app.use(express.static(__dirname + '/static'));
 app.set('bookshelf', bookshelf);
-
-
-var routes = require('./routes')(app);
-var models = require('./models')(app);
-
 
 nunjucks.configure('views', {
   autoescape: true,
   express: app
 });
 
-app.listen(process.env.Port || 8000);
+var routes = require('./routes')(app);
+
+app.listen(process.env.Port || 8000, function () {
+  console.log('App now listening on %s', process.env.Port || 8000);
+});
