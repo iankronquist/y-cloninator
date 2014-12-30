f5b3ef38426f60ffcbf89643bbd5913c05e1e884
Ian Kronquist <iankronquist@teleport.com>
2014-12-29 16:24:24 -0800

    Root route fetches from database

--- routes.js
+++ routes.js
@@ -1,8 +1,9 @@
 module.exports = function(app) {
-
+  var GHProjects = require('./models/ghprojects')(app);
   app.get('/', function (req, res) {
-
-    return res.render('index.hjs', {});
+    new GHProjects().fetchAll().then(function (projects) {
+        return res.render('index.hjs', { projects: projects });
+      }
+    );
 	});
-
 };
