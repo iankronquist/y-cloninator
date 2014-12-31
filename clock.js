09e6408ec62f3b1d84cd43b74d80728ec22145c6
Evan Tschuy <evantschuy@gmail.com>
2014-12-30 19:45:28 -0800

    Log when job starts

--- clock.js
+++ clock.js
@@ -3,6 +3,7 @@ var backend = require('./backend');
 
 // every ten minutes from 8:00-20:00
 var job = schedule.scheduleJob('*/10 8-20 * * *', function () {
+    console.log("Starting job.");
     backend.httpGet(
         backend.hn_api_host,
         '/v0/topstories.json',
