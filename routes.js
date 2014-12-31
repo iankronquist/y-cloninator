824607bff25c01c5b3af988dc5d1829e2b83252c
Ian Kronquist <iankronquist@teleport.com>
2014-12-30 17:58:05 -0800

    Remove bookshelf from routes and add a post filter

--- routes.js
+++ routes.js
@@ -1,9 +1,19 @@
 module.exports = function(app) {
-  var GHProjects = require('./models/ghprojects')(app.get('bookshelf'));
+  var knex = app.get('knex');
   app.get('/', function (req, res) {
-    new GHProjects().fetchAll().then(function (projects) {
-        return res.render('index.hjs', { projects: projects.toJSON() });
-      }
-    );
-	});
+    knex.select('*').from('ghprojects').then(function(projects) {
+      return res.render('index.hjs', { projects: projects });
+    });
+  });
+  app.post('/', function (req, res) {
+    knex.select('*')
+      .from('ghprojects')
+      .where({ gh_language: req.body.language })
+      .then(function (projects) {
+        return res.render('index.hjs', {
+          filter_lang: req.body.language,
+          projects: projects
+        });
+      });
+  });
 };
