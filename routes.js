01e6915b182474429d9ec824a98d41b11465dc3c
Evan Tschuy <evantschuy@gmail.com>
2016-01-21 20:23:54 +0100

    make language clickable

--- routes.js
+++ routes.js
@@ -2,22 +2,33 @@ var backend = require('./backend');
 
 module.exports = function(app) {
   var knex = app.get('knex');
+
+  var searchLanguage = function(res, language) {
+    knex.select('*')
+      .from('ghprojects')
+      .whereRaw('LOWER(gh_language) = LOWER(?)', [language])
+      .then(function (projects) {
+        return res.render('index.hjs', {
+          filter_lang: language,
+          projects: projects
+        });
+      });
+  };
+
   app.get('/', function (req, res) {
     knex.select('*').from('ghprojects').then(function(projects) {
       return res.render('index.hjs', { projects: projects });
     });
   });
-  app.post('/', function (req, res) {
-    knex.select('*')
-      .from('ghprojects')
-      .whereRaw('LOWER(gh_language) = LOWER(?)', [req.body.language])
-      .then(function (projects) {
-        return res.render('index.hjs', {
-          filter_lang: req.body.language,
-          projects: projects
-        });
-      });
+
+  app.get('/:language', function (req, res) {
+    return searchLanguage(res, req.params.language);
   });
+
+  app.post('/', function (req, res) {
+    return searchLanguage(res, req.body.language);
+  });
+
   app.get('/refresh-content', function (req, res) {
     console.log("Starting job.");
     backend.httpGet(
